e5d579ae06bf8cbbd1619ae86c9d1f42cd3c6bc4
Seliesca <seliescawhidyapermata8289@gmail.com>
2021-11-15 19:06:20 +0700

    Add project files.

---
+++ ClientProxyForm-P5-20190140128/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientProxyForm_P5_20190140128
+{
+    public partial class Form1 : Form
+    {
+        ServiceReference1.MatematikaClient obj = new ServiceReference1.MatematikaClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonHasil_Click(object sender, EventArgs e)
+        {
+            int angka1 = Convert.ToInt32(textBox1.Text);
+            int angka2 = Convert.ToInt32(textBox2.Text);
+
+            double hasilTambah = obj.Tambah(angka1, angka2);
+            double hasilKurang = obj.Kurang(angka1, angka2);
+            double hasilKali = obj.Kali(angka1, angka2);
+            double hasilBagi = obj.Bagi(angka1, angka2);
+
+            label3.Text = hasilTambah.ToString();
+            label4.Text = hasilKurang.ToString();
+            label5.Text = hasilKali.ToString();
+            label6.Text = hasilBagi.ToString();
+        }
+    }
+}
+
